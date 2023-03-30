cbd6bce807414f7456b51ff0cfec7488717d9f8f
Mahdi Hasan <mahdihasan10599@gmail.com>
2023-03-31 05:32:30 +0600

    Created the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,21 @@ import './Header.css';
 
 const Header = () => {
     return (
-        
+        <header>
+            <section className='container xl:px-10 py-5 mx-auto flex justify-between items-center border-b border-slate-200'>
+                <h1 className='text-cyan-950 font-bold text-3xl'>Taza Khobor</h1>
+                <div className='flex justify-end gap-20 items-center'>
+                    <div className='flex justify-end items-center gap-6 text-cyan-950 text-lg'>
+                        <a href="" className='hover:text-rose-600'>E-potrika</a>
+                        <a href="" className='hover:text-rose-600'>Live show</a>
+                        <a href="" className='hover:text-rose-600'>Login</a>
+                    </div>
+                    <div>
+                        <img id='logo' src={logo} className="w-12 h-12" alt="" />
+                    </div>
+                </div>
+            </section>
+        </header>
     );
 };
 
